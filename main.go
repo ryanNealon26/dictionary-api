898ec4944ae7c15d8b6e5f2717c31ec003b20930
ryanNealon26 <136101238+ryanNealon26@users.noreply.github.com>
2024-06-11 21:42:38 -0400

    added API endpoint

--- main.go
+++ main.go
@@ -1,52 +1,20 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type word struct {
-	word       string
-	definition string
-}
+func returnDefinition(c *gin.Context) {
+	searchWord := c.Param("searchWord")
+	var dictionary map[int][]Word = loadDictionary()
+	word := handleCollision(dictionary, searchWord)
 
-func hashFunction(word string) int {
-	runeArray := []rune(word)
-	hashKey := 0
-	for i := 0; i < len(runeArray); i++ {
-		hashKey += int(runeArray[i])
-	}
-	return hashKey
-}
-func handleCollision(hashMap map[int][]word, searchWord string) string {
-	mapSize := len(hashMap[hashFunction(searchWord)])
-	for i := 0; i < mapSize; i++ {
-		if searchWord == hashMap[hashFunction(searchWord)][i].word {
-			return hashMap[hashFunction(searchWord)][i].definition
-		}
-	}
-	return "Search Word: " + searchWord + " was not found in the dictionary."
-
-}
-func loadWords() {
-	var dictMap = make(map[int][]word)
-	file, err := os.Open("Dictionary-2.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if len(text) > 2 && strings.Contains(text, "  ") {
-			end := strings.Index(text, "  ")
-			wordKey := word{text[0:end], text[end:]}
-			dictMap[hashFunction(wordKey.word)] = append(dictMap[hashFunction(wordKey.word)], wordKey)
-		}
-	}
+	c.IndentedJSON(http.StatusOK, word.definition)
 }
 func main() {
-	fmt.Println(hashFunction("shelf"))
-	loadWords()
+	router := gin.Default()
+	router.GET("/dictionary/:searchWord", returnDefinition)
+	router.Run("localhost:8080")
 }
